b22a67db9c890590413672a396617f7b4e8e05b6
Gaya <sehja@yandex.ru>
2020-12-03 14:28:29 +0300

    finish slider

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,16 @@
-var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper('.swiper-container-1', {
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
+      },
+    });
+
+var swiper = new Swiper('.swiper-container-2', {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
       },
     });
