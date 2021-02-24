863faacb8ae2b574ac9f779e3bda8be1daba33b7
Gaya <sehja@yandex.ru>
2021-02-24 20:18:33 +0300

    dotes img benefits

--- js/swiper.js
+++ js/swiper.js
@@ -2,6 +2,21 @@ var swiper = new Swiper('.swiper-container-1', {
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
+        slidesPerView: 1,
+        breakpoints: {// настройки для разных разрешений
+            768: {
+                slidesPerView: 'auto',
+                spaceBetween: 8
+            },
+            991: {
+                slidesPerView: 2,
+                spaceBetween: 30
+            },
+            1192: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            }
+        }
       },
     });
 
